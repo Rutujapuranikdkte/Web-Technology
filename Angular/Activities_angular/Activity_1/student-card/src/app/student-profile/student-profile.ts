dbbf138573ce3765880bdffdf77951cd33e48c3e
Rutuja Puranik <rutujapuranik7@gmail.com>
2026-04-29 14:33:55 +0530

    exp

--- Angular/Activities_angular/Activity_1/student-card/src/app/student-profile/student-profile.ts
+++ Angular/Activities_angular/Activity_1/student-card/src/app/student-profile/student-profile.ts
@@ -9,12 +9,12 @@ styleUrl:'./student-profile.css'
 
 export class StudentProfileComponent{
 
-studentName='Divya';
+studentName='Rutuja';
 
 course='AIML';
 
 profileImage=
-'https://via.placeholder.com/150';
+'https://static.vecteezy.com/system/resources/thumbnails/032/176/191/small_2x/business-avatar-profile-black-icon-man-of-user-symbol-in-trendy-flat-style-isolated-on-male-profile-people-diverse-face-for-social-network-or-web-vector.jpg';
 
 changeImage(){
 
